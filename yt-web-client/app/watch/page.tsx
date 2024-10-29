bb43f9d22b06653f28c5906c50ce5c76a4d0a785
Mursalin Habib <mursalinhabib@m.communityfibre.co.uk>
2024-10-29 21:41:00 +0000

    Fixed suspense error

--- yt-web-client/app/watch/page.tsx
+++ yt-web-client/app/watch/page.tsx
@@ -1,15 +1,30 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-export default function Watch() {
+
+ function WatchInternal() {
   const videoPrefix = 'https://storage.googleapis.com/processed-vid-170924/';
   const videoSrc = useSearchParams().get('v');
 
   return (
+    <Suspense fallback= {<div>yoooo</div>}>
     <div>
       <h1>Watch Page</h1>
       { <video controls src={videoPrefix + videoSrc}/> }
     </div>
+    </Suspense>
   );
 }
+
+
+export default function Watch() {
+  
+    return (
+      <Suspense fallback= {<div>yoooo</div>}>
+        <WatchInternal />
+      </Suspense>
+    );
+  }
+
